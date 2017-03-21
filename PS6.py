ff874c191c33276ab104c155a9313fbd6c76798e
Luke Worley <luke.worley@colorado.edu>
2017-03-20 19:15:19 -0600

    still struggling

--- PS6.py
+++ PS6.py
@@ -1,6 +1,6 @@
 import numpy
-x="abc"
-y="ghi"
+x="exponential"
+y="polynomial"
 #add blank spaces
 x= " " + x
 y= " " + y
@@ -19,19 +19,18 @@ def cost(i,j):
 	c_swap=1
 	#print(S)
 	sub=0
-	swap=10000
+	swap=10000 #initializ swap so it isn't chosen until its in bounds (swap doesn't make sense till i=1, j=1
 	print("i is " + str(i) + " x[i] is " + x[i-1] + " j is " + str(j) + " y[j] is " + str(y[j-1]))
+	c_indel1 = c_indel + S[i][j-1]
 
 	if x[i]==y[j]: #sub operation
 		sub=S[i-1][j-1]
 	else:
 		sub=c_sub+S[i-1][j-1]
-
-	#c_indel1 = 100 # c_indel + S[i][j-1]
-	#c_indel2 = 100 #c_indel + S[i][j-1]
+	if j==1:
+		c_indel2 = c_indel + S[i-1][j]
 
 	if j>2 and i>2: #swap op
-		#if x[]
 		swap = 0
 		swap=c_swap+S[i-2][j-2]
 		if x[i-1] != y[j]:
@@ -39,21 +38,23 @@ def cost(i,j):
 		if x[i] != y[j-1]:
 			swap=swap+c_sub
 		#S[i][i] = S[i-2][i-2] + c_swap
-	print ("c_sub is " + str(sub) + " c_swap " + str(swap))# + " c_indel " + str(c_indel))
-	print("min is " + str(min(sub,swap)))#,c_indel1,c_indel2)))
+	print ("c_sub is " + str(sub) + " c_swap " + str(swap) + " c_indel1 " + str(c_indel1) + " c_indel2 " + str(c_indel2) + "\n")
+	#print("min is " + str(min(sub,swap,c_indel2,c_indel1)))#,c_indel1,c_indel2)))
 
-	S[i][j]=min(sub,swap)#,c_indel1,c_indel2)
+	S[i][j]=min(sub,swap,c_indel1,c_indel2)
 
 def alignStrings():
 	#cost(S,1,2)
 	print(S)
 	print(ny)
 	print(nx)
-	for i in range(1,nx):
-		print("i is" + str(i))
-		for j in range (1,ny):
-			print ("j is" + str(j))
-			cost(i,j)
+	i=1
+	for j in range(1,ny):
+		#print("i is" + str(i))
+		cost(i, j)
+		#for j in range (1,ny):
+			#print ("j is" + str(j))
+			#cost(i,j)
 	print(S)
 
 print(alignStrings())

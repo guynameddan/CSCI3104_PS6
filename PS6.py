73c8841d8f01acd0ae8306e8c9af7ea6162c6bb5
Luke Worley <luke.worley@colorado.edu>
2017-03-20 22:37:48 -0600

    print statements are dumb

--- PS6.py
+++ PS6.py
@@ -71,38 +71,3 @@ def alignStrings():
 
 
 print(alignStrings())
-# having trouble with the cost function guys.
-# this is the hardest part of alignStrings.
-# if you read the L7 notes you should be able
-# to understand what I have down.
-#print(alignStrings("Ape","Step"))
-
-
-"""
-	c_indel = 1
-	c_sub = 10
-	c_swap = 0
-
-	# this brings back the value of a swap
-	# based on the character(s) we are looking
-	# at
-	if x[i]=y[j-1] and x[i-1] = y[j]:
-		c_swap = 10
-	if x[i] = y[j-1] and x[i-1] != y[j]:
-		c_swap = 20
-	if x[i] != y [j-1] and x[i-1] = y[j]:
-		c_swap = 20
-	if x[i] != y [j-1] and x[i-1] != y[j]:
-		c_swap = 30
-
-	# this is for two characters that are the same
-	# i.e. a no-op
-	if x[i] = y[j]
-		c_indel = c_sub = c_swap = 0
-
-	C_swap = S[i-2,j-2] + c_swap
-	C_sub = S[i-1,j-1] + c_sub
-	C_indel1 = S[i-1,j] + c_indel
-	C_indel2 = S[i,j-1] + c_indel
-
-	return min(C_swap,C_sub,C_indel1,C_indel2)"""
